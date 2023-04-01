8358f4f2faf034a8d3fe052f6d24d8eea78c8ae5
songxxzp <songxxzp@163.com>
2023-04-01 20:25:03 +0800

    Fix parallel cpu kernel

--- quantization_kernels_parallel.c
+++ quantization_kernels_parallel.c
@@ -17,7 +17,7 @@ void compress_int4_weight(void *weight, void *out, int n, int m)
     {
         for(int j=0;j<m;j++)
         {
-            (*(unsigned char*)(out + sizeof(unsigned char) * (i * m + j))) |= ((*(unsigned char*)(weight + sizeof(unsigned char) * (i * (m << 1) + (j << 1)))) << 4);
+            (*(unsigned char*)(out + sizeof(unsigned char) * (i * m + j))) = ((*(unsigned char*)(weight + sizeof(unsigned char) * (i * (m << 1) + (j << 1)))) << 4);
             (*(unsigned char*)(out + sizeof(unsigned char) * (i * m + j))) |= (((*(unsigned char*)(weight + sizeof(unsigned char) * (i * (m << 1) + ((j << 1) | 1)))) & 15));
         }
     }
